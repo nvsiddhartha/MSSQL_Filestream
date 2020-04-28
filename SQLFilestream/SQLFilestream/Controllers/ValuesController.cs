688369f98bda201b0a696a050d979af7238fc8ea
nvsiddhartha <nvsiddhartha@gmail.com>
2020-04-28 11:00:13 -0400

    get file

--- SQLFilestream/SQLFilestream/Controllers/ValuesController.cs
+++ SQLFilestream/SQLFilestream/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -21,7 +22,43 @@ namespace SQLFilestream.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var files = await context.MyFileStreamTable.Where(i => i.Fsdescription == "capture").ToListAsync();
+            var files = await context.MyFileStreamTable.ToListAsync();
+            return await Task.FromResult(Ok(files.Count()));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var file = context.MyFileStreamTable.Where(i => i.Fsdescription == "capture").FirstOrDefault();
+
+            return File(file.Fsblob, "image/png");
+        }
+
+        //[HttpGet("{id}")]
+        //public async Task<FileContentResult> DownloadAsync(string id)
+        //{
+        //    var file = await context.MyFileStreamTable.Where(i => i.Fsdescription == "capture").FirstOrDefaultAsync();
+
+        //    return new FileContentResult(file.Fsblob, "image/png")
+        //    {
+        //        FileDownloadName = file.Fsdescription
+        //    };
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            var guid = System.Guid.NewGuid();
+            var file = new MyFileStreamTable
+            {
+                Fsid = guid,
+                Fsdescription = "A File " + guid.ToString(),
+                Fsblob = System.IO.File.ReadAllBytes(".//Resources//textfile.txt")
+            };
+
+            context.MyFileStreamTable.Add(file);
+            context.SaveChanges();
+
             return await Task.FromResult(Ok());
         }
     }
